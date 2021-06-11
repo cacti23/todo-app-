48bf86241e8e38f3a4e15ccdd70074cc51d6c88e
aditya patel <patel.aditya2014@gmail.com>
2021-06-12 01:02:18 +0530

    implemented basic functions

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const options = {
 dateElement.innerHTML = `${today.toLocaleDateString('en-US', options)}`;
 
 // creating list data base
-const id = 0;
+let id = 0;
 const storedTaskList = [
   {
     taskName: 'Drink Coffee',
@@ -20,38 +20,34 @@ const storedTaskList = [
 ];
 
 // storing the list in the data base
-localStorage.setItem('storedTaskList', JSON.stringify(storedTaskList));
+// localStorage.setItem('storedTaskList', JSON.stringify(storedTaskList));
 
 // adding to do to the list
 
 const todoList = document.getElementById('todo-list');
 const check = 'far fa-check-circle';
 const uncheck = 'fas fa-check-circle';
+const done = 'line-through';
 
-const addToDo =
-  (todo,
-  id => {
-    const text = `<li class="item">
+const addToDo = (todo, id) => {
+  const text = `<li class="item">
           <i class="co ${check}" jobs="complete" id="${id}"></i>
           <p class="text">${todo}</p>
           <i class="de far fa-trash-alt" jobs="delete" id="${id}"></i>
         </li>`;
-    position = 'beforeend';
+  position = 'beforeend';
 
-    todoList.insertAdjacentHTML(position, text);
-  });
+  todoList.insertAdjacentHTML(position, text);
+};
 
 // adding functionality in the plus button to add the todo item in the list
 const inputElement = document.getElementById('input');
-console.log(inputElement);
-console.log('ji');
 
 document.addEventListener('keyup', e => {
-  console.log(e);
-  if (e.keyCode === 13) {
+  if (e.keyCode == 13) {
     const todo = inputElement.value;
     if (1) {
-      addToDo(todo, id);
+      addToDo(todo, id, false);
       storedTaskList.push({
         taskName: todo,
         taskId: id,
@@ -65,8 +61,20 @@ document.addEventListener('keyup', e => {
 });
 
 // complete todo
+const toggleCompleteTodo = element => {
+  element.classList.toggle(check);
+  element.classList.toggle(uncheck);
 
-document.addEventListener('click', e => {
-  e.target.classList.toggle(check);
-  e.target.classList.toggle(uncheck);
+  element.parentNode.querySelector('.text').classList.toggle(done);
+};
+
+// call complete todo
+window.addEventListener('click', e => {
+  const element = e.target;
+  console.log(element);
+  console.log(element.attributes.job);
+  // const elementJob = element.attribute.job.value;
+  // if (elementJob == 'complete') {
+  //   toggleCompleteTodo();
+  // }
 });
